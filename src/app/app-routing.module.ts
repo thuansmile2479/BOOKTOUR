8ccfc40cdcf24ba6d9f74dad5d75bdb9437b9260
thuannd <thuansmile2479@gmail.com>
2023-06-08 23:40:08 +0700

    feat: display trang admin, notfound refs: BOOKTOUR-7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+ 
+import { LayoutClientComponent } from './layouts/layout-client/layout-client.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LienheComponent } from './pages/lienhe/lienhe.component';
+import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.component';
+import { NotFoundError } from 'rxjs';
+import { BlogComponent } from './pages/blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutClientComponent,
+    children: [
+      { path: '', component: HomeComponent },   
+      { path: 'posts', component: BlogComponent }, 
+      { path: 'lienhe', component: LienheComponent }, 
+
+    ]
+  },
+  {
+    path: 'admin',
+    component: LayoutAdminComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+
+    ]  
+  },
+
+  { path: '**', component: NotFoundError },
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
