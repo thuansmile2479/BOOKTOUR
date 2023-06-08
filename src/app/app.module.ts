78bececbb743f8ddea01958d40cb9b1cd9cfe662
tranvanhuan221 <tranvanhuannr@gmail.com>
2023-06-08 23:18:26 +0700

    feat: giao diện locantion refs: BOOKTOUR-6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component'; 
-import { BlogComponent } from './pages/blog/blog.component'; 
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
 import { LayoutAdminComponent } from './layouts/layout-admin/layout-admin.component';
 import { LayoutClientComponent } from './layouts/layout-client/layout-client.component';
 import { LienheComponent } from './pages/lienhe/lienhe.component';
+import { LocantionComponent } from './pages/locantion/locantion.component';
+import { DetaillocantionComponent } from './pages/locantion/detaillocantion/detaillocantion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent, 
-    BlogComponent, 
+    HomeComponent,
+    BlogComponent,
     LayoutAdminComponent,
     LayoutClientComponent,
-    LienheComponent
+    LienheComponent,
+    LocantionComponent,
+    DetaillocantionComponent,
   ],
   imports: [
     BrowserModule,
