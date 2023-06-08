f464aa8c1dbd9ecc33f0958671f85bec66981f64
thuannd <thuansmile2479@gmail.com>
2023-06-08 22:27:49 +0700

    feat: display liên hệ refs: BOOKTOUR-3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'; 
-import { BlogComponent } from './pages/blog/blog.component';
+import { RouterModule, Routes } from '@angular/router';
 
- 
-import { LayoutClientComponent } from './layouts/layout-client/layout-client.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LienheComponent } from './pages/lienhe/lienhe.component';
-
-const routes: Routes = [
-  {
-    path: '', component: LayoutClientComponent,
-    children: [
-      { path: '', component: HomeComponent },   
-      { path: 'posts', component: BlogComponent }, 
-      { path: 'lienhe', component: LienheComponent }, 
-
-    ]
-  }, 
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
