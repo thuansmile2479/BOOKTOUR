6c86ff8b779d72f96e8645bd5128046ec63f0f45
thuannd <thuansmile2479@gmail.com>
2023-06-10 15:56:55 +0700

    feat: add file catogory, userModel refs: BOOKTOUR-35

---
+++ src/models/userModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+    image: String,
+    role: {
+      type: String,
+      default: "member",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+export default mongoose.model("User", userSchema);
