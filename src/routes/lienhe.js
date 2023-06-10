41241f377097c07b63e1f974e7c11b5f64458860
tranvanhuan221 <tranvanhuannr@gmail.com>
2023-06-10 14:49:00 +0700

    feat: GetAll-Liên hệ refs:BOOKTOUR-28

--- src/routes/lienhe.js
+++ src/routes/lienhe.js
@@ -2,12 +2,13 @@ import express from "express";
 import axios from "axios";
 import {
   addLienhe, 
+  getAllLienhes, 
   getLienheId, 
 } from "../controllers/lienhe.js";
 import { checkPermission } from "../middlewares/checkPermission.js";
 const router = express.Router();  
 
-
+router.get("/lienhes", getAllLienhes);
 router.get("/lienhes/:id", getLienheId);
 router.post("/lienhes", addLienhe);  
 
