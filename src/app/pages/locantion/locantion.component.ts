a268912964334150fb65d8e833cc83731f83a201
dao <dao24252003@gmail.com>
2023-06-10 17:03:52 +0700

    feat: render-front-location
    refs: BOOKTOUR-36

--- src/app/pages/locantion/locantion.component.ts
+++ src/app/pages/locantion/locantion.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-locantion',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./locantion.component.css']
 })
 export class LocantionComponent {
+
+  products: any[] | undefined;
+  product: any = [];
+  constructor(private productService: ProductService, private router: Router) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(
+      (response) => {
+        this.products = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  navigateToDetail(productId: string) {
+    this.router.navigate(['/products/' + productId]);
+  }
+
+
+  
+  deleteProduct(id: number | string) {
+    this.productService.deleteProduct(id).subscribe(() => {
+      this.getProducts();
+      confirm('Bạn có chắc muốn xóa không');
+    });
+  } 
+
 
 }
