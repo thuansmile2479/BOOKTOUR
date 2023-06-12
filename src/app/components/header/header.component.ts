970b0c4f72affb16ed9e9f484a0f4f4bbb5fefc8
dao <dao24252003@gmail.com>
2023-06-12 10:41:59 +0700

    feat: check admin-user refs: BOOKTOUR-44

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnChanges, OnInit {
+  UserName = '';
+  login=false;
+  ngOnInit(): void {
+    var isLogin = localStorage.getItem('user') as string;
+      var user = JSON.parse(isLogin);
+      console.log(user);
+      
+      if(user) {
+        this.login = true;
+        this.UserName = user.user.name;
+      }
 
+      console.log(this.UserName);
+      
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes) {
+      var isLogin = localStorage.getItem('user');
+      console.log(isLogin);
+      
+      // if(isLogin) {
+      //   this.UserName = JSON.stringify(isLogin)
+      // }
+    }
+  }
 }
